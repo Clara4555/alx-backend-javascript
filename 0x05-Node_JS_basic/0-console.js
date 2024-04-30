8194d9ccbec66a55d5826118d617e424e2ab6a54
Clara4555 <favourloyalty8@gmail.com>
2024-04-30 15:43:17 -0700

    task

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ *  function named displayMessage that prints in 
+ *STDOUT the string argument.
+ * @param {String}`
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
